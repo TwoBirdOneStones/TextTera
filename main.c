df223b9017f8ac2d64d3f7843440f52cfb4eee53
TwoStoneOneBirds <dsvenice98@gmail.com>
2025-04-21 19:40:49 -0400

    nothing here yet

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#define WORLD_WIDTH 25
+#define WORLD_HEIGHT 25
+
+enum BlockType {
+  Air = 0,
+  Dirt = 1,
+  Stone = 2,
+};
+
+typedef struct {
+  int x;
+  int y;
+} Transform;
+
+typedef struct {
+  enum BlockType type;
+  Transform transform;
+} Block;
+
+Block blocks[WORLD_WIDTH][WORLD_HEIGHT];
 
 int main(){
   printf("Hello World\n");
-    // Nothing here yet.
+
+
+
   return 0;
- }
+}
+
+
